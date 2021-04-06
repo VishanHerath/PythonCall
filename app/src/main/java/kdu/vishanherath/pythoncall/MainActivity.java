a3d2db37c332f28131c8a9d47ad25ef8f2300be4
VishanHerath <vishanherath1998@gmail.com>
2021-04-06 07:03:18 +0530

    First

---
+++ app/src/main/java/kdu/vishanherath/pythoncall/MainActivity.java
@@ -0,0 +1,20 @@
+package kdu.vishanherath.pythoncall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+    }
+}
